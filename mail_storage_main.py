5ca2e196bbf2355f04538a888ac55a7cc274c24e
akshay <akshaynaik797@gmail.com>
2021-02-13 19:10:16 +0530

    0.0.5

--- mail_storage_main.py
+++ mail_storage_main.py
@@ -2,7 +2,8 @@ from temp import mail_mover, mail_storage
 
 def process_mails_in_range(hospital, fromtime, totime, deferred):
     mail_storage(hospital, fromtime, totime, deferred)
-    mail_mover(hospital, deferred)
+    if deferred != 'X':
+        mail_mover(hospital, deferred)
 
 if __name__ == '__main__':
     mail_storage("ils_agartala", "01/02/2021 12:00:00", "02/02/2021 12:00:00", "X")
